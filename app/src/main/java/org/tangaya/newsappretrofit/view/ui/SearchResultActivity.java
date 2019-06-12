d6a7f913416340deb829adec456651bb374e59d4
Amal Qurany <mal.qurany@gmail.com>
2019-06-13 06:56:50 +0700

    add search in appbar

--- app/src/main/java/org/tangaya/newsappretrofit/view/ui/SearchResultActivity.java
+++ app/src/main/java/org/tangaya/newsappretrofit/view/ui/SearchResultActivity.java
@@ -1,14 +1,18 @@
 package org.tangaya.newsappretrofit.view.ui;
 
-import android.app.ActivityOptions;
 import android.app.ProgressDialog;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import org.tangaya.newsappretrofit.R;
@@ -27,14 +31,17 @@ public class SearchResultActivity extends AppCompatActivity {
     private ArticleAdapter adapter;
     private RecyclerView recyclerView;
     ProgressDialog progressDialog;
+    String searchKeyword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_result);
 
-        String searchKeyword = getIntent().getExtras().getString("keyword");
+        searchKeyword = getIntent().getExtras().getString("keyword");
         setTitle("Cari Berita: " + searchKeyword);
+
+        handleIntent(getIntent());
 
         progressDialog = new ProgressDialog(SearchResultActivity.this);
         progressDialog.setMessage("mengambil data artikel...");
@@ -66,6 +73,23 @@ public class SearchResultActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+
+            Toast.makeText(getApplicationContext(), "search for " + query, Toast.LENGTH_SHORT).show();
+            if (searchKeyword == null) {
+                searchKeyword = query;
+            }
+        }
+    }
+
     private void generateDataList(final APIResponse apiResponse) {
 
         Log.d("SearchResultActivity", "articleList size: " + apiResponse.getArticles().size());
@@ -88,5 +112,21 @@ public class SearchResultActivity extends AppCompatActivity {
             }
         }));
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
+    }
+
 
 }
